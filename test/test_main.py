56d85fe8e46a1d3a9202ffd7ad97b065fdef65ec
Xuan Canh <xuancanhit99@gmail.com>
2023-01-17 12:26:35 +0300

    Update

--- test/test_main.py
+++ test/test_main.py
@@ -8,36 +8,36 @@ def test_healthcheck():
     assert response.status_code == 200
 
 
-# class TestAuthors():
-#     def test_get_empty_author(self):
-#         response = requests.get(f'{api_url}/api/authors')
-#         assert response.status_code == 200
-#         assert len(response.json()) == 0
+class TestAuthors():
+    def test_get_empty_author(self):
+        response = requests.get(f'{api_url}/api/authors')
+        assert response.status_code == 200
+        assert len(response.json()) == 0
 
-#     def test_create_author(self):
+    def test_create_author(self):
 
-#         body = {
-#             "name": "Leanne Graham",
-#             "username": "Bret",
-#             "email": "Sincere@april.biz",
-#             "address": "Vietnam"
-#         }
-#         response = requests.post(f'{api_url}/api/add-author', json=body)
-#         assert response.status_code == 200
-#         assert response.json().get('name') == 'Leanne Graham'
-#         assert response.json().get('username') == 'Bret'
-#         assert response.json().get('email') == 'Sincere@april.biz'
-#         assert response.json().get('address') == 'Vietnam'
+        body = {
+            "name": "Leanne Graham",
+            "username": "Bret",
+            "email": "Sincere@april.biz",
+            "address": "Vietnam"
+        }
+        response = requests.post(f'{api_url}/api/add-author', json=body)
+        assert response.status_code == 200
+        assert response.json().get('name') == 'Leanne Graham'
+        assert response.json().get('username') == 'Bret'
+        assert response.json().get('email') == 'Sincere@april.biz'
+        assert response.json().get('address') == 'Vietnam'
 
-#     def test_get_author_by_id(self):
-#         response = requests.get(f'{api_url}/api/author/0')
-#         assert response.status_code == 200
-#         assert response.json().get('name') == 'Leanne Graham'
-#         assert response.json().get('username') == 'Bret'
-#         assert response.json().get('email') == 'Sincere@april.biz'
-#         assert response.json().get('address') == 'Vietnam'
+    def test_get_author_by_id(self):
+        response = requests.get(f'{api_url}/api/author/0')
+        assert response.status_code == 200
+        assert response.json().get('name') == 'Leanne Graham'
+        assert response.json().get('username') == 'Bret'
+        assert response.json().get('email') == 'Sincere@april.biz'
+        assert response.json().get('address') == 'Vietnam'
 
-#     def test_get_not_empty_author(self):
-#         response = requests.get(f'{api_url}/api/authors')
-#         assert response.status_code == 200
-#         assert len(response.json()) == 1
+    def test_get_not_empty_author(self):
+        response = requests.get(f'{api_url}/api/authors')
+        assert response.status_code == 200
+        assert len(response.json()) == 1
